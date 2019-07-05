578948bd3d972417d625c1c20333beb45208c191
2395566058 <2395566058@qq.com>
2019-07-05 13:43:26 +0800

    sad

--- commons/src/main/java/keilen/gdkm/weixin/service/WeiXinProxy.java
+++ commons/src/main/java/keilen/gdkm/weixin/service/WeiXinProxy.java
@@ -34,6 +34,7 @@ public class WeiXinProxy {
 
 		String url = "https://api.weixin.qq.com/cgi-bin/user/info" + "?scope=snsapi_userinfo&access_token=" + accessToken + "&openid="
 				+ openId + "&lang=zh_CN";
+		LOG.trace("调用远程接口：\n{}", url);
 		HttpRequest request = HttpRequest.newBuilder(URI.create(url)).GET().build();
 		try {
 			HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString(Charset.forName("UTF-8")));
