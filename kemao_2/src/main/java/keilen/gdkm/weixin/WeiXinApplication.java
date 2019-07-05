416baf540128e95388b67ae8f808058be13afc6a
2395566058 <2395566058@qq.com>
2019-07-05 12:50:11 +0800

    dsa

--- kemao_2/src/main/java/keilen/gdkm/weixin/WeiXinApplication.java
+++ kemao_2/src/main/java/keilen/gdkm/weixin/WeiXinApplication.java
@@ -1,16 +1,12 @@
 package keilen.gdkm.weixin;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.context.annotation.ComponentScan;
 
-import keilen.gdkm.weixin.domain.InMessage;
 
 @SpringBootApplication
+@ComponentScan("keilen.gdkm")
 public class WeiXinApplication implements CommonsConfig{
 
 	public static void main(String[] args) {
