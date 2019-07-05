585ef03d77e81fda5275291faeadb0a86ef3e37d
2395566058 <2395566058@qq.com>
2019-07-05 13:09:43 +0800

    c

--- kemao_2/src/main/java/keilen/gdkm/weixin/controller/MessageReceiverController.java
+++ kemao_2/src/main/java/keilen/gdkm/weixin/controller/MessageReceiverController.java
@@ -1,16 +1,12 @@
 package keilen.gdkm.weixin.controller;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 
+import keilen.gdkm.weixin.domain.InMessage;
+import keilen.gdkm.weixin.service.MessageConvertHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JsonParseException;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,10 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-
-import keilen.gdkm.weixin.domain.InMessage;
-import keilen.gdkm.weixin.service.MessageConvertHelper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 @RestController
 @RequestMapping("/kemao/MessageReceiver")
@@ -31,34 +26,46 @@ public class MessageReceiverController {
 	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
 
 	@Autowired
-	private RedisTemplate<String, ? extends InMessage> inMessaageTemplate;
+	private XmlMapper xmlMapper;
+	@Autowired
+	private RedisTemplate<String, ? extends InMessage> inMessageTemplate;
 
 	@GetMapping
-	public String echo(@RequestParam("signature") String signature, @RequestParam("timestamp") String timestamp,
-			@RequestParam("nonce") String nonce, @RequestParam("echostr") String echostr) {
+	public String echo(//
+			@RequestParam("signature") String signature, //
+			@RequestParam("timestamp") String timestamp, //
+			@RequestParam("nonce") String nonce, //
+			@RequestParam("echostr") String echostr//
+	//
+	) {
 		return echostr;
 	}
 
 	@PostMapping
-	public String getMessage(@RequestBody String xml) throws IOException {
-		LOG.trace("收到消息原文：\n{}\n", xml);
-		InMessage inMessage = MessageConvertHelper.convert(xml);
-		LOG.debug("转换后的消息对象：\n{}\n", inMessage);
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		ObjectOutputStream out = new ObjectOutputStream(bos);
-		out.writeObject(inMessage);
-		byte[] data = bos.toByteArray();
-		String channel = "kemao_2_" + inMessage.getMsgType();
-		inMessaageTemplate.execute(new RedisCallback<InMessage>() {
+	public String onMessage(//
+			@RequestParam("signature") String signature, //
+			@RequestParam("timestamp") String timestamp, //
+			@RequestParam("nonce") String nonce, //
+			@RequestBody String xml) throws JsonParseException, JsonMappingException, IOException {
+		LOG.trace("收到的消息原文：\n{}\n------------------------------", xml);
+		InMessage inMessage = convert(xml);
+		if (inMessage == null) {
+			LOG.error("消息无法转换！原文：\n{}\n", xml);
+			return "success";
+		}
 
-			@Override
-			public InMessage doInRedis(RedisConnection connection) throws DataAccessException {
-				connection.publish(channel.getBytes(), data);
-				return null;
-			}
-		});
-		inMessaageTemplate.convertAndSend(channel, inMessage);
+		LOG.debug("转换后的消息对象\n{}\n", inMessage);
+
+		String channel = "kemao_2_" + inMessage.getMsgType();
+		inMessageTemplate.convertAndSend(channel, inMessage);
+
 		return "success";
 	}
 
+	private InMessage convert(String xml) throws JsonParseException, JsonMappingException, IOException {
+		Class<? extends InMessage> c = MessageConvertHelper.getClass(xml);
+		InMessage msg = xmlMapper.readValue(xml, c);
+		return msg;
+	}
 }
+
